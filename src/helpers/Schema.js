b837817b10adebe0bc7fa31b8e7a8f9a949ed7a4
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-09-21 03:00:02 +0200

    feat(signup-endpoint):Implement Signup api Endpoint -created the endpoint for the Feature -wrote all neccesary tests for the feature and it's various components -implemented authentication,hashing and validation. [Finishes #168614767]

---
+++ src/helpers/Schema.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import {stringRegex, emailRegex, passwordRegex} from "./regex";
+export const Signup = {
+    firstName: Joi.string().required().min(1).max(30).regex(stringRegex),
+    lastName:  Joi.string().required().min(1).max(30).regex(stringRegex),
+    email:     Joi.string().required().email().regex(emailRegex),
+    password:  Joi.string().required().min(6).max(30).regex(passwordRegex),
+    gender:    Joi.string().required().min(1).max(6),
+    jobRole:   Joi.string().required().min(1).max(30),
+    department: Joi.string().required().min(1).max(30),
+    address:    Joi.string().required().min(1).max(30),
+    isAdmin:    Joi.boolean()
+};
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import User from '../controllers/User';
+import Validator from '../middleware/Valdiator';
+
+
+const userRoutes = express.Router();
+
+userRoutes.post('/auth/signup',Validator.validateSignup, User.signUp);
+
+
+
+export default userRoutes;
