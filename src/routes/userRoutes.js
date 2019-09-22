ec2766c21f4a449e00e2f6519f4e2944de021010
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-09-23 01:01:55 +0200

    feat(signin):Intialised login validator

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import User from '../controllers/User';
-import Validator from '../middleware/Valdiator'
-
+import {validateSignup} from '../middleware/SignupValidator'
 
 const userRoutes = express.Router();
 
-userRoutes.post('/auth/signup', Validator.validateSignup, User.signUp);
+userRoutes.post('/auth/signup', validateSignup, User.signUp);
 
 
 
