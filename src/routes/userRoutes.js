48737571aecf14d184d648377e19574886536efd
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-09-23 01:07:41 +0200

    Setup route for signin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import User from '../controllers/User';
-import {validateSignup} from '../middleware/SignupValidator'
+import {validateSignup} from '../middleware/SignupValidator';
+import {validateSignin} from '../middleware/SigninValidator';
 
 const userRoutes = express.Router();
 
 userRoutes.post('/auth/signup', validateSignup, User.signUp);
+
+userRoutes.post('/auth/signin', validateSignin);
 
 
 
