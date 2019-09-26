965c73bcb5c01d5d3c74d4c0eb7b1fa00099d92f
kwizera elvis <44610648+kwizeraelvis@users.noreply.github.com>
2019-09-26 20:25:43 +0200

    Update NewArticleValidator.js

--- src/middleware/NewArticleValidator.js
+++ src/middleware/NewArticleValidator.js
@@ -3,6 +3,6 @@ import {ArticleSchema} from '../helpers/Schemas/NewArticleSchema';
 
 export const validateNewArticle = (req, res, next) => {
         const {error} = joi.validate(req.body, ArticleSchema);
-        if(error){return res.status(422).send({status: 422, error: error.details[0].message})};
+        if(error)return res.status(422).send({status: 422, error: error.details[0].message});
         next();
 };
