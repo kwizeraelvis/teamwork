57f222b9a13c0205b10fc6b86084486b7ed0a6fc
kwizera elvis <44610648+kwizeraelvis@users.noreply.github.com>
2019-10-04 06:23:21 +0200

    Update DeleteArticle.js

--- src/controllers/DeleteArticle.js
+++ src/controllers/DeleteArticle.js
@@ -4,5 +4,5 @@ export const DeleteanArticle = (req, res) => {
     const article = ArticleModal.findArticle(req.params.articleId)
     if(!article){return res.status(404).send({status: 404, message: 'Article not found'});}
     ArticleModal.deleteArticle(req.params.articleId);
-    res.status(204).send({status: 204, message: 'Article was succesfully deleted'})
+    return res.status(204).send({status: 204, message: 'Article was succesfully deleted'})
 }
